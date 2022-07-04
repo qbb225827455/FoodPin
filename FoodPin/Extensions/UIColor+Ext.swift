018ad36820ee88a080a49f8c9dab04347a280fea
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-07-04 23:14:50 +0800

    UIColor extension

--- FoodPin/Extensions/UIColor+Ext.swift
+++ FoodPin/Extensions/UIColor+Ext.swift
@@ -5,4 +5,15 @@
 //  Created by 陳鈺翔 on 2022/7/4.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        
+        let redVal = CGFloat(red) / 255
+        let greenVal = CGFloat(green) / 255
+        let blueVal = CGFloat(blue) / 255
+        
+        self.init(red: redVal, green: greenVal, blue: blueVal, alpha: 1)
+    }
+}
