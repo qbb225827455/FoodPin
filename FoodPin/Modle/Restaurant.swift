9073b865be6f1f904103d3dbf457b5fd141e3746
YU-SIANG,CHEN <qbb225827455@gmail.com>
2022-07-08 00:40:07 +0800

    Converted to CoreData

--- FoodPin/Modle/Restaurant.swift
+++ FoodPin/Modle/Restaurant.swift
@@ -6,8 +6,25 @@
 //
 
 import Foundation
+import CoreData
 
-struct Restaurant: Hashable {
+public class Restaurant: NSManagedObject {
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Restaurant> {
+        return NSFetchRequest<Restaurant>(entityName: "Restaurant")
+    }
+
+    @NSManaged public var name: String
+    @NSManaged public var type: String
+    @NSManaged public var location: String
+    @NSManaged public var phone: String
+    @NSManaged public var summary: String
+    @NSManaged public var image: Data
+    @NSManaged public var ratingText: String?
+    @NSManaged public var isFavorite: Bool
+    
+}
+
+extension Restaurant {
     
     enum Rating: String {
         case awesome
@@ -32,12 +49,17 @@ struct Restaurant: Hashable {
         }
     }
     
-    var name: String = ""
-    var type: String = ""
-    var location: String = ""
-    var phone: String = ""
-    var description: String = ""
-    var image: String = ""
-    var isFavorite: Bool = false
-    var rating: Rating?
+    var rating: Rating? {
+        get {
+            guard let ratingText = ratingText else {
+                return nil
+            }
+            
+            return Rating(rawValue: ratingText)
+        }
+        
+        set {
+            self.ratingText = newValue?.rawValue
+        }
+    }
 }
